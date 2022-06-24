77fa1a9891951e89aacdcb6f92c56472ea501f54
Mürsel <murselcetin51@gmail.com>
2022-06-24 23:40:55 +0300

    Drawer Menü eklendi. Toolbar güncellendi.

--- app/src/main/java/com/example/forlike/MainActivity.kt
+++ app/src/main/java/com/example/forlike/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.forlike
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.forlike.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        NavigationUI.setupWithNavController(binding.navigationView, navHostFragment.navController)
+
+
+
+        val toogle = ActionBarDrawerToggle(this, binding.drawer, binding.toolbar, 0, 0)
+        binding.drawer.addDrawerListener(toogle)
+        toogle.syncState()
+
+        val header = binding.navigationView.inflateHeaderView(R.layout.drawer_header)
     }
 }
